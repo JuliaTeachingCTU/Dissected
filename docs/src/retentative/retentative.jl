323ea32af2a10daed5a3e2d99a0b21a24f787b1b
Tomas Pevny <pevnak@gmail.com>
2024-10-16 22:09:22 +0200

    Added linear attention

--- docs/src/retentative/retentative.jl
+++ docs/src/retentative/retentative.jl
@@ -243,16 +243,66 @@ end
 
 # With that, the batch forward function will look like, which is neet and fast (but allocates a lot with which we will deal later)
 function batch_forward(m::RetentionLayer, x)
-    Qs = rotary_embedding(m.Wq * x, -m.θₐ)
-    Ks = rotary_embedding(m.Wk * x, -m.θₐ)
+    Q = rotary_embedding(m.Wq * x, -m.θₐ)
+    K = rotary_embedding(m.Wk * x, -m.θₐ)
     v = m.Wv * x
-    D = DMatrix(m.γ, size(Qs, 2));
-    transpose(((Qs' * Ks) .* D) * v')
+    D = DMatrix(m.γ, size(Q, 2));
+    transpose(((Q' * K) .* D) * v')
 end
 
 # Does it work? Let's test the equivalence of `batch_forward` and `recursive_forward`.
 
-m = RetentionLayer(6, 8)
+layer = RetentionLayer(6, 8)
 x = randn(Float32, 6, 33)
 
-recursive_forward(m, x) ≈ batch_forward(m, x)
+recursive_forward(layer, x) ≈ batch_forward(layer, x)
+
+# ### Decresing the memory footprint
+# 
+# The `batch_forward` has quite high memory footprint, because of the outer
+# product `(Q * V ).* D`. We can decrease the memory footprint using the 
+# map
+function batch_forward2(layer::RetentionLayer, x)
+    Q = rotary_embedding(layer.Wq * x, -layer.θₐ)
+    K = rotary_embedding(layer.Wk * x, -layer.θₐ)
+    v = layer.Wv * x
+    γ = layer.γ
+    linear_attention(Q, K, v, γ)
+end
+
+function linear_attention(Q, K, v, γ)
+    o = map(1:length(v)) do n 
+        @inbounds sum(only( γ ^(n - m) * dot(view(Q, :, n), view(K, :, m)) * v[m]) for m in 1:n)
+    end
+    o'
+end
+
+
+layer = RetentionLayer(128, 128)
+x = randn(Float32, 128, 512)
+
+batch_forward(layer, x) ≈ batch_forward2(layer,x)
+
+@benchmark batch_forward(layer, x)
+@benchmark batch_forward2(layer,x)
+
+# The `batch_forward2` is slower, but has much lower memory footprint. 
+# This is because the `batch_forward` uses multi-threadding, while 
+# `batch_forward2` uses only one thread. Let's try multi-threadding
+
+function linear_attention(Q, K, v, γ)
+    o = zeros(eltype(v), 1, size(x,2))
+    Threads.@threads :dynamic for n in axes(v,2)
+        @inbounds for m in 1:n
+            o[n] += only( γ ^(n - m) * dot(view(Q, :, n), view(K, :, m)) * v[m])
+        end
+    end
+    o
+end
+
+@benchmark batch_forward3(layer,x)
+
+# With the implementation is equally fast as the `batch_forward`, 
+# but with much lower memory footprint.
+
+
